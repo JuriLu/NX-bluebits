7707930ede014ab27b5283e9e1be4a035e1d7b76
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-25 15:14:48 +0200

    General

--- bluebits/apps/admin/src/app/app.module.ts
+++ bluebits/apps/admin/src/app/app.module.ts
@@ -7,9 +7,12 @@ import { RouterModule } from "@angular/router";
 import { appRoutes } from "./app.routes";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" })],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, NxWelcomeComponent],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes, {initialNavigation: "enabledBlocking"})],
+    providers: [],
+    exports: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- bluebits/apps/ngshop/src/app/app.module.ts
+++ bluebits/apps/ngshop/src/app/app.module.ts
@@ -5,10 +5,24 @@ import { AppComponent } from "./app.component";
 import { NxWelcomeComponent } from "./nx-welcome.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { ProductListComponent } from "./pages/product-list/product-list.component";
+import { RouterModule, Routes } from "@angular/router";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent
+  },
+  {
+    path: "products",
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
