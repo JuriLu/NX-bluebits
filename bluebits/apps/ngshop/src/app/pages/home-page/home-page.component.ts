5f6e8e044ad510489a9b7fb7637436e4bd7be42e
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-25 15:14:33 +0200

    ESlint configs name changes

--- bluebits/apps/ngshop/src/app/pages/home-page/home-page.component.ts
+++ bluebits/apps/ngshop/src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "bluebits-home-page",
+  selector: "ngshop-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"]
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 }
